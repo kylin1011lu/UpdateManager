e79dc4c9381580640e148018aa0ede3af80b9936
luqilin <luqilin@4417.com>
2018-07-17 15:20:22 +0800

    luac自动解密对比工具

---
+++ ZonstGitDiff/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ZonstGitDiff
+{
+    class Program
+    {
+        //C:\Users\dall\AppData\Local\Temp\TortoiseGit\game_create_room_view-60e9c4e.002.luac C:\Users\dall\AppData\Local\Temp\TortoiseGit\game_create_room_view-74c1c2d.002.luac
+        //C:\Users\dall\AppData\Local\Temp\TortoiseGit\game_create_room_view-74c1c2d.002.luac
+
+        static void Main(string[] args)
+        {
+            if(args.Length < 2)
+            {
+                Console.WriteLine("args num error");
+                return;
+            }
+
+            for(int i = 0; i < args.Length; ++i)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+            string newPath0 = Tool.DecodeLuac(args[0]);
+            string newPath1 = Tool.DecodeLuac(args[1]);
+
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = "TortoiseGitMerge.exe";
+            process.StartInfo.Arguments = newPath0 + " " + newPath1;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+            process.Start();
+        }
+    }
+}
---
+++ ZonstGitDiff/Tool.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;//使用DllImport需要这个头文件
+
+namespace ZonstGitDiff
+{
+    class Tool
+    {
+        [DllImport("LuaDecode.dll", EntryPoint = "stringcheck", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void stringcheck([MarshalAs(UnmanagedType.LPStr)]string x, [MarshalAs(UnmanagedType.LPStr)]string y, [Out, MarshalAs(UnmanagedType.LPStr)]StringBuilder z, int len);
+
+        [DllImport("LuaDecode.dll", EntryPoint = "decodeluac", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int decodeluac([MarshalAs(UnmanagedType.LPStr)]string infile, [MarshalAs(UnmanagedType.LPStr)]string sign, [MarshalAs(UnmanagedType.LPStr)]string key, [Out, MarshalAs(UnmanagedType.LPStr)]StringBuilder z, int len);
+
+        public static string DecodeLuac(string path)
+        {
+
+            if (File.Exists(path))
+            {
+                FileInfo fi1 = new FileInfo(path);
+                if(fi1.Length == 0)
+                {
+                    return path;
+                }
+
+                int LEN = 4096;
+                int times = 1;
+                int ret = 0;
+                StringBuilder _builder;
+                do
+                {
+                    _builder = new StringBuilder(LEN * times);
+                    ret = decodeluac(path, "XXTEA", "2dxLua", _builder, _builder.Capacity);
+                    times++;
+                } while (ret == 1);
+
+                // Create a file to write to.
+                string newPath = fi1.DirectoryName + "\\decode_" + fi1.Name;
+                using (StreamWriter sw = File.CreateText(newPath))
+                {
+                    sw.Write(_builder);
+                }
+
+                //Console.WriteLine(_builder);
+                return newPath;
+            }
+            return path;
+        }
+
+    }
+}
